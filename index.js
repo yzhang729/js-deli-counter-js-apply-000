8e3e9779da13937ba7ce992706d9bbcf302ce26c
Yuting Zhang <yutingzhang729@gmail.com>
2019-02-08 22:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function takeANumber(katzDeliLine, newCust){
   katzDeliLine.push(newCust);
-  return `Welcome, ${newCust}. You are number ${katzDeliLine.length+1} in line.`;
+  return `Welcome, ${newCust}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine){
